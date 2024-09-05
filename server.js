ffc6417afdf1ebdc8abc75dfc4c7c01c33277aff
Mohit <pa.mohit47@gmail.com>
2024-09-06 01:05:58 +0530

    PORT

--- server.js
+++ server.js
@@ -83,7 +83,8 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT => {
-    console.log(`App is listening on ${PORT}`);
+server.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
     connectDB();
-})
+   }
+    );
